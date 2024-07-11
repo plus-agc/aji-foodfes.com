4c4d4f8e4a2856c116178e390b5e751af22b2624
plus-agc <plus.web.dev.team@gmail.com>
2024-07-11 16:10:42 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import relativeLinks from "astro-relative-links";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [relativeLinks()],
+  integrations: [relativeLinks(), sitemap()],
   output: "static",
   compressHTML: false,
   build: {
     // 例: ビルド時に`page/index.html`ではなく`page.html`を生成します。
     format: "file",
   },
+  site: "https://aji-foodfes.com",
 });
