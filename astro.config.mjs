add5fb48efbab84d1f3b4827bd0eaa46d9d53ac5
plus-agc <plus.web.dev.team@gmail.com>
2024-07-11 10:43:22 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [relativeLinks()]
+  integrations: [relativeLinks()],
+  output: "static",
+  compressHTML: false,
+  build: {
+    // 例: ビルド時に`page/index.html`ではなく`page.html`を生成します。
+    format: "file",
+  },
 });
