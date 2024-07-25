0ac03f572bcc6376b39ed1c6d8a453422b1c4f1b
plus-agc <plus.web.dev.team@gmail.com>
2024-07-25 09:29:27 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import relativeLinks from "astro-relative-links";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
@@ -10,7 +9,6 @@ export default defineConfig({
     relativeLinks(),
     sitemap(),
     partytown({
-      // add
       config: {
         forward: ["dataLayer.push"],
       },
@@ -19,7 +17,6 @@ export default defineConfig({
   output: "static",
   compressHTML: false,
   build: {
-    // 例: ビルド時に`page/index.html`ではなく`page.html`を生成します。
     format: "file",
   },
   site: "https://aji-foodfes.com",
