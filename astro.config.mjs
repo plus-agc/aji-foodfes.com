b11a588415454ed7e8294e6c319e7fff3bc9fc17
plus-agc <plus.web.dev.team@gmail.com>
2024-08-02 13:17:22 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,19 @@
 import { defineConfig } from "astro/config";
 import relativeLinks from "astro-relative-links";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
-
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     relativeLinks(),
     sitemap(),
-    robotsTxt(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
     robotsTxt({
       sitemap: [
         "https://aji-foodfes.com/sitemap-0.xml",
         "https://aji-foodfes.com/sitemap-index.xml",
       ],
     }),
-    sentry(),
-    spotlightjs(),
   ],
   output: "static",
   compressHTML: false,
